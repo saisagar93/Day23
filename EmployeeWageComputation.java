b1ed0c35af26a3c8dc212b87d8e2f5d331eef34a
saisagar93 <sscreator225@gmail.com>
2020-12-26 16:44:56 +0530

    Calculate emp Wages using Method

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,23 @@
 public class EmployeeWageComputation
 {
-	//Constants
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int MAX_WORKING_HRS=100;
 
-	public static void main(String[] args)					//Main method
+	public static int computeEmpwage()
 	{
-
 		System.out.println("Welcome to employee wage computation");
-		//Variables
 		int empHrs=0;
 		int totalEmpWage=0;
 		int empWage=0;
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
-		//while loop for less than 20 working days and less than 100 working hours
 		while(totalEmpHrs<=MAX_WORKING_HRS && totalWorkingDays<=NUM_OF_WORKING_DAYS)
 		{
 			totalWorkingDays++;
-			int empCheck=(int)Math.floor(Math.random()*10)%3;	//To generate a random valueof 0 to 2
+			int empCheck=(int)Math.floor(Math.random()*10)%3;
 
 			switch(empCheck)
 			{
@@ -40,5 +36,11 @@ public class EmployeeWageComputation
 		}
 		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;                       //Salary of an employee
 		System.out.println("Total Employee Wage: "+totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args)
+	{
+		computeEmpwage();
 	}
 }
+
