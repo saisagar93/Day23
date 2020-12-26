60f486d0453511268ddb596c775c0b7dd752897e
saisagar93 <sscreator225@gmail.com>
2020-12-26 16:49:29 +0530

    Multiple Companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,46 +1,48 @@
-public class EmployeeWageComputation
-{
-	public static final int FULL_TIME=1;
-	public static final int PART_TIME=2;
-	public static final int EMP_RATE_PER_HR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_WORKING_HRS=100;
+class EmpWageSalary {
+        //constant
+        public static final int IS_FULL_TIME = 1;
+        public static final int IS_PART_TIME = 2;
 
-	public static int computeEmpwage()
-	{
-		System.out.println("Welcome to employee wage computation");
-		int empHrs=0;
-		int totalEmpWage=0;
-		int empWage=0;
-		int totalEmpHrs=0;
-		int totalWorkingDays=0;
-		while(totalEmpHrs<=MAX_WORKING_HRS && totalWorkingDays<=NUM_OF_WORKING_DAYS)
-		{
-			totalWorkingDays++;
-			int empCheck=(int)Math.floor(Math.random()*10)%3;
+        public void empSalary(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth ) 
+                   {
+                                //variables
+				int empHrs = 0;
+				int totalEmpHrs = 0;
+				int totalWorkingDays = 0;
+				while (totalEmpHrs <= maxHrPerMonth && totalWorkingDays < numberOfWorkingDays) 
+                                 {
+					totalWorkingDays++;
+					double empCheck = Math.floor(Math.random() * 10) % 3;
+                                 //Switchcase
+			        switch ((int)empCheck)
+                                {
+                                case IS_FULL_TIME :
+                                        empHrs = 8;
+                                        break;
+                                case IS_PART_TIME :
+                                        empHrs = 4;
+                                        break;
+                                default :
+                                        empHrs = 0;
 
-			switch(empCheck)
-			{
-				case FULL_TIME:
-				empHrs=8;
-				break;
+                                }
+						totalEmpHrs += empHrs;
+						System.out.println(totalWorkingDays+" "+totalEmpHrs);
+				}
 
-				case PART_TIME:
-				empHrs=4;
-				break;
+                        int totalEmpWage = totalEmpHrs * empRatePerHr;
+                        System.out.println(company+" "+totalEmpWage);
 
-				default:
-				empHrs=0;
-			}
-			totalEmpHrs+=empHrs;					//Total working hours
-		}
-		totalEmpWage=totalEmpHrs*EMP_RATE_PER_HR;                       //Salary of an employee
-		System.out.println("Total Employee Wage: "+totalEmpWage);
-		return totalEmpWage;
-	}
-	public static void main(String[] args)
-	{
-		computeEmpwage();
-	}
+        }//empSalary()
 }
+  public class EmployeeWageComputation
+      {
 
+        public static void main(String[] args) 
+           {
+		System.out.println("Welcome To Compute Wage Problrem");
+                EmpWageSalary empWage = new EmpWageSalary();
+                empWage.empSalary("Realiance", 20, 20, 100);
+		empWage.empSalary("HDFC", 50, 20, 100);
+           }
+}
